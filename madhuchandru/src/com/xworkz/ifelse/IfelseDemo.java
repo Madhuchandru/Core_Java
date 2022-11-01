3dee741bc42f79c20752cc294e74c303bc09ced9
madhu chandru <madhuchandru2000@gmail.com>
2022-11-01 23:02:19 +0530

    control flow stmt

---
+++ madhuchandru/src/com/xworkz/ifelse/IfelseDemo.java
@@ -0,0 +1,14 @@
+package com.xworkz.ifelse;
+
+public class IfelseDemo {
+	public static void main(String[] args) {  
+	    int age=20;  
+	    if(age>18){  
+	        System.out.print("Age is greater than 18");  
+	    }  
+	    else {
+	    	System.out.println("Age is lesser than 18");
+	    }
+	}  
+	}  
+
--- madhuchandru/src/com/xworkz/madhuchandru/examples/Raghu.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.madhuchandru.examples;
-
-public class Raghu {
-
-	public static void main(String[] args) {
-		System.out.println("RAGHU");
-	}
-
-}
